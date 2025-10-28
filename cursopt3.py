151b2bc9c4a44d34c628e45a948cb0cb2d879a24
jaozinndev <joaopaulocoelho760@gmail.com>
2025-10-28 00:21:56 -0300

    esse é facinho

---
+++ cursopt3.py
@@ -0,0 +1,10 @@
+num1 = int(input("digite um numero : "))
+num2 = int(input("digite outro numero: "))
+if num1 > num2 :
+    print(f"o primeiro numero {num1} é maior que {num2 }")
+elif num1 == num2 :
+    print("os numeros sao iguais ")
+else :
+    print(f"o segundo numero {num2 } é maior que {num1}")
+
+   
